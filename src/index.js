24da60bbc8885752ef2152674649aae7472c32fa
Alan Souza <alansouzati@gmail.com>
2016-10-24 17:36:53 -0700

    Added exports for each function.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import getDocAsJSON from './getDocAsJSON';
 import getDocAsMarkdown from './getDocAsMarkdown';
 import PropTypes from './PropTypes';
 
+exports.docComponent = docComponent;
+exports.docPropType = docPropType;
+exports.getDocAsJSON = getDocAsJSON;
+exports.getDocAsMarkdown = getDocAsMarkdown;
+exports.PropTypes = PropTypes;
+
 export default {
-  docComponent, docPropType, getDocAsJSON, getDocAsMarkdown, PropTypes
+  docComponent, docPropType, getDocAsJSON, getDocAsMarkdown, PropTypes,
 };
