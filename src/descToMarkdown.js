57ffcab696e6de80df5963b772d991d7ed0e477f
Alan Souza <alansouzati@gmail.com>
2018-10-30 22:18:44 -0700

    ADded an extra space for the detailed description

--- src/descToMarkdown.js
+++ src/descToMarkdown.js
@@ -23,7 +23,7 @@ function getAvailableAt({ availableAt }) {
 
 function getHeader({ description, details, deprecated, name }) {
   return `## ${deprecated ? `~~${name}~~` : name}${deprecated ? ` (${deprecated})` : ''}
-${description}${details ? `\n${details}` : ''}\n`;
+${description}${details ? `\n\n${details}` : ''}\n`;
 }
 
 function getUsage({ usage }) {
